57170e3a9f6ed800965eea90d255cde2969458cb
QuinnTracy1129 <92907201+QuinnTracy1129@users.noreply.github.com>
2023-02-20 12:01:28 +0800

    login and register done

--- controllers/Persons/Users.js
+++ controllers/Persons/Users.js
@@ -3,51 +3,23 @@ const User = require("../../models/Persons/Users");
 // entity/
 exports.browse = (req, res) =>
   User.find()
+    .select("-password")
     .sort({ createdAt: -1 })
-    .select("-password")
-    .then(users => res.json(users.filter(user => !user.deletedAt)))
-    .catch(error => res.status(400).json({ error: error.message }));
-
-// entity/unresolved
-exports.unresolved = (req, res) =>
-  User.find()
-    .select("-password")
-    .populate("fullName.mname fullName.lname")
-    .then(users =>
-      res.json(
-        users
-          .filter(user => !user.deletedAt && !user.role)
-          .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-      )
-    )
+    .then(users => res.json(users.filter(item => !item.deletedAt)))
     .catch(error => res.status(400).json({ error: error.message }));
 
 // entity/archive
 exports.archive = (req, res) =>
   User.find()
     .select("-password")
-    .populate("fullName.mname fullName.lname")
-    .then(items =>
-      res.json(
-        items
-          .filter(item => item.deletedAt)
-          .sort((a, b) => new Date(b.deletedAt) - new Date(a.deletedAt))
-      )
-    )
-    .catch(error => res.status(400).json({ error: error.message }));
-
-// entity/:id/find
-exports.find = (req, res) =>
-  User.findById(req.params.id)
-    .select("-password")
-    .then(user => res.json(user.deletedAt ? "No user found" : user))
+    .sort({ createdAt: -1 })
+    .then(items => res.json(items.filter(item => item.deletedAt)))
     .catch(error => res.status(400).json({ error: error.message }));
 
 // entity/:id/update
 exports.update = (req, res) => {
   User.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
-    populate: "fullName.mname fullName.lname",
   })
     .select("-password")
     .then(item => res.json(item))
--- models/Persons/Users.js
+++ models/Persons/Users.js
@@ -40,6 +40,10 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+userSchema.query.byAvailable = function (deletedAt) {
+  return this.where({ deletedAt });
+};
+
 userSchema.methods.matchPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
